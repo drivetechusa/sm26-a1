f93663a9dc4e0266f0b1ce92ab446d3c9549fa8e
drivetechusa <jimbreen@drivetechusa.com>
2026-01-13 17:52:06 -0500

    add section for letter maintenance

--- resources/views/pages/letters/⚡index.blade.php
+++ resources/views/pages/letters/⚡index.blade.php
@@ -19,9 +19,9 @@ new class extends Component {
 <div class="space-y-4">
 
     <p>Letter List</p>
-    <div class="flex gap-2">
-        <flux:input wire:model.live.debounce.2s="search" placeholder="Search" icon="magnifying-glass"/>
-    </div>
+{{--    <div class="flex gap-2">--}}
+{{--        <flux:input wire:model.live.debounce.2s="search" placeholder="Search" icon="magnifying-glass"/>--}}
+{{--    </div>--}}
     <div class="w-full">
         <flux:table :paginate="$this->letters">
             <flux:table.columns>
