1c1768740e0793f56265c9be2335b8144af75207
drivetechusa <jimbreen@drivetechusa.com>
2026-01-14 08:14:56 -0500

    fix enrollments

--- app/Livewire/Forms/EnrollmentForm.php
+++ app/Livewire/Forms/EnrollmentForm.php
@@ -2,14 +2,15 @@
 
 namespace App\Livewire\Forms;
 
+use App\Models\Seminar;
 use App\Models\Student;
 use Livewire\Attributes\Validate;
 use Livewire\Form;
 
 class EnrollmentForm extends Form
 {
-    public Student $student;
-    public ?int $seminar = null;
+    public ?Student $student;
+    public ?int $seminar_id = null;
     public ?float $discount = null;
     public ?string $level = null;
     public ?float $tuition = null;
@@ -17,10 +18,18 @@ class EnrollmentForm extends Form
     public function rules(): array
     {
         return [
-            'seminar_id' => ['required', 'integer', 'exists:seminars,id'],
+            'seminar_id' => ['required', 'integer', 'exists:classes,id'],
             'discount' => ['nullable', 'numeric', 'min:0'],
             'level' => ['required', 'string', 'max:255'],
             'tuition' => ['required', 'numeric', 'min:0'],
         ];
+    }
+
+    public function store()
+    {
+        $this->validate();
+        $seminar = Seminar::find($this->seminar_id);
+        $this->student->seminars()
+            ->attach($seminar, ['discount' => $this->discount, 'level' => $this->level, 'tuition' => $this->tuition]);
     }
 }
