ce4ac63a7f516ad284e62c09d999eccb0da6802a
drivetechusa <jimbreen@drivetechusa.com>
2026-01-27 09:06:02 -0500

    add quarter income report

--- resources/views/pages/reports/⚡index.blade.php
+++ resources/views/pages/reports/⚡index.blade.php
@@ -17,6 +17,14 @@ new class extends Component {
     public $workzone_end;
     public $payroll_start;
     public $payroll_end;
+    public $current_year;
+    public $quarter;
+    public $quarter_year;
+
+    public function mount()
+    {
+        $this->current_year = today()->year;
+    }
 
     public function runTptReport()
     {
@@ -81,6 +89,18 @@ new class extends Component {
         return (new \App\Exports\WorkzoneSafetyExport($this->workzone_start, $this->workzone_end))->download('workzone_safety_' . time() . '.xlsx');
 
     }
+
+    public function quarter_income_report()
+    {
+        $this->validate([
+            'quarter' => 'required',
+            'quarter_year' => 'required'
+        ]);
+        $pdf = $this->quarterly_income_report($this->quarter, $this->quarter_year);
+        return response()->streamDownload(function () use ($pdf) {
+            $pdf->Output();
+        }, 'quarterly-income-report.pdf');
+    }
 };
 ?>
 
@@ -143,18 +163,30 @@ new class extends Component {
         </flux:card>
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
-{{--        <flux:card class="space-y-6">--}}
-{{--            <div>--}}
-{{--                <flux:heading size="lg">Payroll</flux:heading>--}}
-{{--            </div>--}}
-{{--            <div class="space-y-6">--}}
-{{--                <flux:input label="Start" type="date" wire:model="payroll_start"/>--}}
+        <flux:card class="space-y-6">
+            <div>
+                <flux:heading size="lg">Quarterly Income</flux:heading>
+            </div>
+            <div class="space-y-6">
+                <flux:select wire:model="quarter" label="Quarter">
+                    <flux:select.option>Select Quarter...</flux:select.option>
+                    <flux:select.option value="1">Jan - Mar</flux:select.option>
+                    <flux:select.option value="2">Apr - Jun</flux:select.option>
+                    <flux:select.option value="3">Jul - Sep</flux:select.option>
+                    <flux:select.option value="4">Oct - Dec</flux:select.option>
+                </flux:select>
+                <flux:select wire:model="quarter_year" label="Year">
+                    <flux:select.option>Select Year...</flux:select.option>
+                    @for ($i = 0; $i <= 10; $i++)
+                        <flux:select.option value="{{$current_year - $i}}">{{$current_year - $i}}</flux:select.option>
+                    @endfor
+                </flux:select>
 
-{{--            </div>--}}
-{{--            <div class="space-y-2">--}}
-{{--                <flux:button variant="primary" class="w-full" wire:click="runPayrollReport">Run Report</flux:button>--}}
-{{--            </div>--}}
-{{--        </flux:card>--}}
+            </div>
+            <div class="space-y-2">
+                <flux:button variant="primary" class="w-full" wire:click="quarter_income_report">Run Report</flux:button>
+            </div>
+        </flux:card>
 
         {{--        <flux:card class="space-y-6">--}}
         {{--            <div>--}}
