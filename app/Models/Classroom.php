7f8d9fea995ab29dee3d2533da39f236febb4888
drivetechusa <jimbreen@drivetechusa.com>
2026-07-21 18:57:17 -0400

    fix btw contract

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -10,6 +10,8 @@ class Classroom extends Model
     /** @use HasFactory<\Database\Factories\ClassroomFactory> */
     use HasFactory;
 
+    protected $guarded = [];
+
     public function casts()
     {
         return [
